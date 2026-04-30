d87fdebd98198cc33366ed565186e3095914db6a
a25eriatimol <a25eriatimol@hINFO11-D3.xiam.cat>
2026-04-30 10:51:35 +0200

    He acabat amb  el apartat del estat de una incidencia el qual tenia un petit problema, també he fet el apartat de revisar totes les incidencies que ja estan finalitzades, i he començat a acabar amb el header fent el apartat que queda (ajuda)

--- incidencies_finalitzades.php
+++ incidencies_finalitzades.php
@@ -2,21 +2,20 @@
 
 <link rel="stylesheet" href="css/header.css">
 <link rel="stylesheet" href="css/styles.css">
-    <title>GI3P - Tècnic</title>
-    <h2>Veure Totes</h2>
-
+    <title>GI3P - Usuari</title>
+    <h2>Incidencies finalitzades</h2>
 <?php
 require_once 'connexio.php';
 ?>
 <?php
-$id = $_POST['id_incidencia'];
-$sql = "SELECT * FROM incidencies";
+$sql = "SELECT * FROM incidencies WHERE data_fi IS NOT NULL";
 $result = $conn->query($sql);
 if ($result->num_rows > 0) {
     while ($row = $result->fetch_assoc()) {
-        echo $row["departament_id"] 
-            .$row["data_ini"]   
-            .$row["prioritat"]      
+        echo $row["id_inc"] 
+            .$row["departament_id"]   
+            .$row["data_ini"]      
+            .$row["data_fi"]
             .$row["tecnic_id"];
         echo " <a href='esborrar.php?id=" . $row["id_inc"] . "'>Esborrar</a></p>";
     }
